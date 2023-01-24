5cd69b8623abcc41d82c698754c2bfdc99a70f0d
MarcosDanielM <mdmgn09@gmail.com>
2023-01-24 19:16:46 -0300

    Lista de items

--- src/app/mock-task.ts
+++ src/app/mock-task.ts
@@ -4,13 +4,13 @@ export const TASKS: Task[] = [
       "id": 1,
       "text": "Terminar primer modulo de Angular",
       "day": "Agosto 5 a las 12:00",
-      "reminder": false
+      "reminder": true
     },
     {
       "id": 2,
       "text": "Hacer las compras para la cena",
       "day": "Agosto 5 a las 17:00",
-      "reminder": false
+      "reminder": true
     },
     {
       "id": 3,
